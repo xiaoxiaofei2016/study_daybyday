d0d4c051c75230940a185dab0cc5066c6aa4e023
dufajun <dfj163004>
2021-10-24 11:49:08 +0800

    大小写改变

--- react_TS/ts-react-app/src/index.tsx
+++ react_TS/ts-react-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './components/demo/Hello'
+import Hello from './components/Demo/Hello'
 
 ReactDOM.render(
   <Hello name="TypeScript" />,
