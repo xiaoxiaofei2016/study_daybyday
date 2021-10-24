20813deb4bca1eec81950288b80ab39a7b94a553
dufajun <dfj163004>
2021-10-24 12:18:01 +0800

    重新提交

--- react_TS/ts-react-app/src/index.tsx
+++ react_TS/ts-react-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './components/Demo/Hello'
+import Hello from './components/demo/Hello'
 
 ReactDOM.render(
   <Hello name="TypeScript" />,
